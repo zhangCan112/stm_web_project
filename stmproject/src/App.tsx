b28ebd5ff106d74fae18dde4fcf48d1aeed020ff
zhangcan <zhangcan@xuaoshouyi.com>
2019-08-01 07:50:59 +0800

    配置了路由

--- stmproject/src/App.tsx
+++ stmproject/src/App.tsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
 import './App.css';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history'
+const browserHistory = createBrowserHistory()
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Router  history={browserHistory}>
+          <Route path='/abc' component={Button} />
+        </Router>
         <Button type="primary">Button</Button>
       </div>
     );
