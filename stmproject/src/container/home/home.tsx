13c563df571e8604df2bf3a82d41071cb2b65d72
zhangcan <zhangcan@xuaoshouyi.com>
2019-09-12 08:15:55 +0800

    添加主页右边按钮设置UI

--- stmproject/src/container/home/home.tsx
+++ stmproject/src/container/home/home.tsx
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Layout, Icon, Avatar } from 'antd';
 import styles from './home.module.css';
 import { NavLink } from 'react-router-dom';
 const { Header, Content, Footer } = Layout;
 
 export default class Home extends Component {
-    
+
     render() {
         return (
             <Layout>
-                <Header className={styles.header} style={{background: "#4A90E2"}}>
+                <Header className={styles.header} style={{ background: "#4A90E2" }}>
                     <div className={styles.logo} />
                     <div className={styles.navlinksBox}>
-                    <NavLink to="/home" className={styles.navLinkNormal} activeStyle={{opacity:1}}>工作台</NavLink>
-                    <NavLink to="/home1" className={styles.navLinkNormal} activeStyle={{opacity:1}}>日报</NavLink>
-                    <NavLink to="/home2" className={styles.navLinkNormal} activeStyle={{opacity:1}}>周报</NavLink>
-                    <NavLink to="/home3" className={styles.navLinkNormal} activeStyle={{opacity:1}}>回顾</NavLink>
-                    </div> 
-                    <div>
-                        用户设置部分
-                    </div>                                       
+                        <NavLink to="/home" className={styles.navLinkNormal} activeStyle={{ opacity: 1 }}>工作台</NavLink>
+                        <NavLink to="/home1" className={styles.navLinkNormal} activeStyle={{ opacity: 1 }}>日报</NavLink>
+                        <NavLink to="/home2" className={styles.navLinkNormal} activeStyle={{ opacity: 1 }}>周报</NavLink>
+                        <NavLink to="/home3" className={styles.navLinkNormal} activeStyle={{ opacity: 1 }}>回顾</NavLink>
+                    </div>
+                    <div className={styles.setting}>
+                        <Icon type="search" className={styles.settingIcon} />
+                        <Icon type="bell" className={styles.settingIcon} />
+                        <div className={styles.userInfo}>
+                            <Avatar className={styles.avatar} icon="user" size={32} />
+                            <span className={styles.userName}>User Name</span>
+                        </div>
+                    </div>
                 </Header>
-                <Content style={{ padding: '0 50px', marginTop: 64 }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+                <Content style={{ padding: '0 50px', marginTop: 24 }}>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 530 }}>Content</div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Footer style={{ textAlign: 'center' }}>©2019 风林火山技术部出品</Footer>
             </Layout>
         )
     }
