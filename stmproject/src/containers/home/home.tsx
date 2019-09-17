7829bdf7f94bb2f106b6cb53199eb7d2eedf5505
zhangcan <zhangcan@xiaoshouyi.com>
2019-09-17 13:23:48 +0800

    优化Home组件UI

--- stmproject/src/containers/home/home.tsx
+++ stmproject/src/containers/home/home.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Icon, Avatar } from 'antd';
+import { Layout, Icon, Avatar, Button } from 'antd';
 import css from './home.module.css';
 import { NavLink } from 'react-router-dom';
 const { Header, Content, Footer } = Layout;
@@ -10,7 +10,10 @@ export default class Home extends Component {
         return (
             <Layout style={{height: '100vh'}}>
                 <Header className={css.header} style={{ background: "#4A90E2" }}>
-                    <div className={css.logo} />
+                    <div className={css.logo} >
+                    <img  alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" width="32px" height="32px"/>
+                    <span className={css.logoTitle}>管理自己的时间</span>
+                    </div>
                     <div className={css.navlinksBox}>
                         <NavLink to="/workbench" className={css.navLinkNormal} activeStyle={{ opacity: 1 }}>工作台</NavLink>
                         <NavLink to="/home1" className={css.navLinkNormal} activeStyle={{ opacity: 1 }}>日报</NavLink>
@@ -18,12 +21,12 @@ export default class Home extends Component {
                         <NavLink to="/home3" className={css.navLinkNormal} activeStyle={{ opacity: 1 }}>回顾</NavLink>
                     </div>
                     <div className={css.setting}>
-                        <Icon type="search" className={css.settingIcon} />
-                        <Icon type="bell" className={css.settingIcon} />
-                        <div className={css.userInfo}>
+                        <Icon type="search" className={css.settingIcon} onClick={()=>{}}/>                      
+                        <Icon type="bell" className={css.settingIcon} onClick={()=>{}}/>
+                        <Button className={css.userInfo} onClick={()=>{}} ghost>
                             <Avatar className={css.avatar} icon="user" size={32} />
                             <span className={css.userName}>User Name</span>
-                        </div>
+                        </Button>
                     </div>
                 </Header>
                 <Content className={css.contentBox}>
