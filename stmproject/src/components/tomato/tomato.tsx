087f9dd5206b6a57906391de3d2bdaa4e540ff67
zhangcan <zhangcan@xuaoshouyi.com>
2019-09-25 08:04:03 +0800

    完成倒计时函数

--- stmproject/src/components/tomato/tomato.tsx
+++ stmproject/src/components/tomato/tomato.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Timeline, Icon, Avatar, Button, Popover, message } from 'antd';
+import { Timeline, Icon, Progress, Button, Popover, message } from 'antd';
+import { countdown } from "../../utils/tools";
 import css from './tomato.module.css';
 
 
@@ -8,15 +9,29 @@ interface IProps {
 }
 
 interface IState {
-
+    count: number
 }
 
 export default class Tomato extends Component<IProps, IState> {
+    
+    state = {
+        count: 0
+    }
+
+    constructor(props: IProps) {
+        super(props)   
+        countdown((lefts)=>{
+            console.log("====>",lefts)
+            this.setState({count: lefts})
+        }, 30, 1000)          
+    }    
+
     render() {
         return (
             <div className={css.container}>
-                <Button className={css.button}>
-                    <Icon type="caret-right" />开始FeedBack12345
+                
+                <Button className={css.button}>                 
+                <Icon type="caret-right" />开始FeedBack1234{this.state.count}
                 </Button>
                 <div>2018年03月18日</div>
                 <Timeline pending="正在进行FeedBack12345" reverse={true}>
@@ -24,7 +39,7 @@ export default class Tomato extends Component<IProps, IState> {
                     <Timeline.Item>10:30-11:20 紧急bug：Feedback-12315</Timeline.Item>
                     <Timeline.Item>13:30-14:20 多文档上传 </Timeline.Item>
                     <Timeline.Item>14:30-15:20 团队成员继承 </Timeline.Item>
-                </Timeline>,
+                </Timeline>                
             </div>
         )
     }
--- stmproject/src/utils/tools.ts
+++ stmproject/src/utils/tools.ts
@@ -1,10 +1,41 @@
+import { number } from "prop-types";
 
 
 
+/**
+ * 延时毫秒执行
+ * @param millisecond 延时的毫秒数
+ */
 export const delay = async (millisecond: number) => {
      await new Promise((resovle) => {
         setTimeout(()=>{
             resovle()
         }, millisecond)
     })
+}
+
+
+/**
+ * 倒计时
+ * @param handler 每次间隔的回调
+ * @param count   总计时次数
+ * @param interval 计时间隔
+ * @returns 终止倒计时的执行函数（stop）
+ */
+export const countdown = (handler:(lefts: number)=>void, count: number, interval: number = 1000) => {
+    let start = new Date().getTime()    
+    let end = new Date(start + (count * interval)).getTime()    
+    let intervalHandler = setInterval(()=>{
+        let now = new Date().getTime()
+        let lefts = end - now
+        if ((lefts) < (interval * 0.5)) {
+            //should end
+            clearInterval(intervalHandler)
+            handler(0)
+            return            
+        }        
+        handler(Math.ceil(lefts/interval))
+    }, interval)
+
+    return ()=>{clearInterval(intervalHandler)}
 }
