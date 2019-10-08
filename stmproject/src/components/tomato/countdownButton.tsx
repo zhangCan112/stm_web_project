ad1fa408a66f1fb80045c488f27e353fc7b3ae0e
zhangcan <zhangcan@xuaoshouyi.com>
2019-10-09 07:47:50 +0800

    番茄工作按钮

--- stmproject/src/components/tomato/countdownButton.tsx
+++ stmproject/src/components/tomato/countdownButton.tsx
@@ -1,51 +1,183 @@
 import React, { Component } from 'react';
-import { Icon, Button} from 'antd';
+import { Icon, Button } from 'antd';
 import { countdown } from "../../utils/tools";
 import css from './tomato.module.css';
 
+enum CountdownStage {
+    /**
+     * 未开始
+     */
+    Normal,
+    /**
+     * 工作
+     */
+    Working,
+    /**
+     * 休息
+     */
+    Resting,
+}
 
 interface IProps {
-    
+
     /**
-     * 开始
+     * 开始的标题内容
      */
     title: string,
     /**
-     * 设置的倒计时时长，单位分钟
+     * 工作倒计时时长，单位分钟,默认25分钟
      */
-    minutes: number,
+    workMinutes: number,
     /**
-     * 倒计时结束时的回调
-     * @param isTerminated 是否被人为打断而终止 True：人为终止 False: 自然结束
-     * @param completedMilliseconds 已完成的时间，单位为毫秒
+     * 休息倒计时时长，单位分钟，默认5分钟
      */
-    onEnd?: (isTerminated: boolean ,completedMilliseconds: number)=>void,    
+    restMinutes: number,
+    /**
+     * 倒计时被终止时的回调
+     * @param stage 被终止时的状态阶段     
+     */
+    onTerminated?: (stage: CountdownStage) => void,
+    /**
+     * 倒计时正常结束时的回调
+     * @param stage 被终止时的状态阶段     
+     */
+    onEnd?: (stage: CountdownStage) => void,
 }
 
 interface IState {
-    count: number
-    isEnd: boolean
+    stage: CountdownStage,
+    leftSeconds?: number,
 }
 
 export default class CountdownButton extends Component<IProps, IState> {
-         
+
+    /**
+     * 计时器终止句柄
+     */
+    timeHandler?: () => void
+
     constructor(props: IProps) {
         super(props)
         this.state = {
-            count: 0,
-            isEnd: true,
+            stage: CountdownStage.Normal,
         }
     }
 
     render() {
+        
         return (
             <Button className={css.button} onClick={this.onClick}>
-                    <Icon type="caret-right" />开始FeedBack1234{this.state.count}
+                <Icon type={this.getButtonIcon()} />{this.getButtonTitle()}
             </Button>
         )
     }
 
     onClick = () => {
-
+        switch (this.state.stage) {
+            case CountdownStage.Working:
+                this.terminateWork()
+                break;
+            case CountdownStage.Resting:
+                this.terminateRest()
+                break;
+            default:
+                this.startWork()
+                break;
+        }
     }
+
+    startWork = () => {        
+        this.timeHandler = countdown(
+            (lefts: number) => {
+                this.setState({
+                    stage: CountdownStage.Working,
+                    leftSeconds: lefts
+                })
+                if (lefts == 0) {  
+                    this.endWork()
+                    this.startRest()
+                }                
+             },
+            this.props.workMinutes * 60,
+            1000)
+    }
+
+    startRest = () => {                    
+        this.timeHandler = countdown(
+            (resetLefts: number) => {
+                this.setState({
+                    stage: CountdownStage.Resting,
+                    leftSeconds: resetLefts
+                })                            
+            },
+            this.props.restMinutes * 60,
+            1000)
+    }  
+
+    endWork = () => {
+        this.timeHandler &&  this.timeHandler()
+        this.props.onEnd && this.props.onEnd(CountdownStage.Working)                
+    }
+    
+    endRest = () => {
+        this.timeHandler &&  this.timeHandler()
+        this.props.onEnd && this.props.onEnd(CountdownStage.Resting)
+        this.setState({
+            stage: CountdownStage.Normal,
+        })        
+    }
+
+
+    terminateWork = () => {
+        this.timeHandler &&  this.timeHandler()
+        this.props.onTerminated && this.props.onTerminated(CountdownStage.Working)
+        this.setState({
+            stage: CountdownStage.Normal,
+        })
+    }
+
+    terminateRest = () => {
+        this.timeHandler &&  this.timeHandler()
+        this.props.onTerminated && this.props.onTerminated(CountdownStage.Resting)
+        this.setState({
+            stage: CountdownStage.Normal,
+        })
+    }
+
+    getButtonTitle = () => {
+        let btnTitle = `开始${this.props.title}`
+        let lefts = this.state.leftSeconds || 0
+        switch (this.state.stage) {
+            case CountdownStage.Working:
+                btnTitle = `处理${this.props.title}...${this.secondsToTime(lefts)}`
+                break;
+            case CountdownStage.Resting:
+                btnTitle = `休息中`
+                break;
+            default:
+                btnTitle = `开始${this.props.title}`
+                break;
+        }
+        return btnTitle;
+    }
+
+    getButtonIcon = () => {             
+        switch (this.state.stage) {
+            case CountdownStage.Working:
+                return "loading";   
+            case CountdownStage.Resting:
+                return "clock-circle";   
+            default:
+                return "play-circle";                
+        }        
+    }
+
+
+    secondsToTime = (seconds: number) => {    
+        let mintinues = Math.floor(seconds/60)
+        let restSeconds = seconds%60
+        return  (mintinues == 0  ? '' : `${Math.ceil(seconds/60)}分`) + 
+                (seconds%60 == 0 ? '' : `${seconds%60}秒`)
+    }
+
 }
--- stmproject/src/components/tomato/tomato.tsx
+++ stmproject/src/components/tomato/tomato.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Timeline, Icon, Progress, Button, Popover, message } from 'antd';
 import { countdown } from "../../utils/tools";
+import CountdownButton from "./countdownButton";
 import css from './tomato.module.css';
 
 
@@ -9,29 +10,23 @@ interface IProps {
 }
 
 interface IState {
-    count: number
+    
 }
 
 export default class Tomato extends Component<IProps, IState> {
 
     state = {
-        count: 0
+        
     }
 
     constructor(props: IProps) {
-        super(props)
-        countdown((lefts) => {
-            console.log("====>", lefts)
-            this.setState({ count: lefts })
-        }, 30, 1000)
+        super(props)        
     }
 
     render() {
         return (
             <div className={css.container}>                                                
-                <Button className={css.button}>
-                    <Icon type="caret-right" />开始FeedBack1234{this.state.count}
-                </Button>
+                <CountdownButton title={'Feedback1234'} workMinutes={1} restMinutes={1}/>
                 <div>2018年03月18日</div>
                 <Timeline pending="正在进行FeedBack12345" reverse={true}>
                     <Timeline.Item >9:00-10:20 活动记录草稿 </Timeline.Item>
