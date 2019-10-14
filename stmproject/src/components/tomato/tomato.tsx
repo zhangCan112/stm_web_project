76f7ac2b50728700d770f35378c05e177dc1c026
zhangcan <zhangcan@xiaoshouyi.com>
2019-10-14 17:40:57 +0800

    番茄工作法模块UI初步完成

--- stmproject/src/components/tomato/tomato.tsx
+++ stmproject/src/components/tomato/tomato.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Timeline, Icon, Progress, Button, Popover, message } from 'antd';
+import { Timeline, List, Icon, Progress, Button, Popover, message } from 'antd';
 import { countdown } from "../../utils/tools";
 import CountdownButton from "./countdownButton";
 import css from './tomato.module.css';
+import InfiniteScroll from 'react-infinite-scroller'
 
 
 interface IProps {
@@ -10,30 +11,52 @@ interface IProps {
 }
 
 interface IState {
-    
+
 }
 
 export default class Tomato extends Component<IProps, IState> {
 
     state = {
-        
+
     }
 
     constructor(props: IProps) {
-        super(props)        
+        super(props)
     }
 
     render() {
         return (
-            <div className={css.container}>                                                
-                <CountdownButton title={'Feedback1234'} workMinutes={1} restMinutes={1}/>
-                <div>2018年03月18日</div>
-                <Timeline pending="正在进行FeedBack12345" reverse={true}>
-                    <Timeline.Item >9:00-10:20 活动记录草稿 </Timeline.Item>
-                    <Timeline.Item>10:30-11:20 紧急bug：Feedback-12315</Timeline.Item>
-                    <Timeline.Item>13:30-14:20 多文档上传 </Timeline.Item>
-                    <Timeline.Item>14:30-15:20 团队成员继承 </Timeline.Item>
-                </Timeline>
+            <div className={css.container}>
+                <CountdownButton title={'Feedback1234'} workMinutes={1} restMinutes={1} />
+                <div className={css.recordList}>
+                    <InfiniteScroll
+                        initialLoad={false}
+                        pageStart={0}
+                        loadMore={()=>{}}
+                        hasMore={false}
+                        useWindow={false}
+                    >
+                        <List
+                            dataSource={[1, 2]}
+                            renderItem={item => (
+                                <List.Item>
+                                    <div>
+                                        <div className={css.titleBox}>
+                                            <span className={css.dateTitle}>2018年03月18日</span>
+                                            <span className={css.resultTitle}>完成了3个番茄🍅</span>
+                                        </div>
+                                        <Timeline pending={item==1 ? "FeedBack12345": undefined} reverse={true}>
+                                            <Timeline.Item >9:00-10:20 活动记录草稿 </Timeline.Item>
+                                            <Timeline.Item>10:30-11:20 紧急bug：Feedback-12315</Timeline.Item>
+                                            <Timeline.Item>13:30-14:20 多文档上传 </Timeline.Item>
+                                            <Timeline.Item>14:30-15:20 团队成员继承 </Timeline.Item>
+                                        </Timeline>
+                                    </div>
+                                </List.Item>
+                            )}
+                        />
+                    </InfiniteScroll>
+                </div>
             </div>
         )
     }
